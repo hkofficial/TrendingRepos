7bae04bf0718cab1754312a511d032ff5a226f2e
Hamza Khan <hamza.khan@tilismtech.com>
2021-10-26 02:34:13 +0500

    Added retry view Implemented repository item layout.

--- app/src/main/java/com/hk/trendingrepos/MainActivity.kt
+++ app/src/main/java/com/hk/trendingrepos/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.hk.trendingrepos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retryImageView = findViewById<ImageView>(R.id.retry_image_view)
+        Glide.with(this).load(R.drawable.retry_gif).into(retryImageView)
     }
 }
