d090710a78b939a1d7e37855d0ca0b1523c3e0fa
Hamza Khan <hamza.khan@tilismtech.com>
2021-10-27 19:47:50 +0500

    Enabled data binding Added shimmer view for loading state.

--- app/src/main/java/com/hk/trendingrepos/MainActivity.kt
+++ app/src/main/java/com/hk/trendingrepos/MainActivity.kt
@@ -3,12 +3,16 @@ package com.hk.trendingrepos
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
+import com.hk.trendingrepos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val retryImageView = findViewById<ImageView>(R.id.retry_image_view)
         Glide.with(this).load(R.drawable.retry_gif).into(retryImageView)
