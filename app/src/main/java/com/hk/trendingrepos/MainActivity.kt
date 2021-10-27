32915fe2a3080594a583d4294cc6f178975bd7ce
Hamza Khan <hamza.khan@tilismtech.com>
2021-10-28 03:58:44 +0500

    Added swipe to refresh layout. Fixed retry button not working.

--- app/src/main/java/com/hk/trendingrepos/MainActivity.kt
+++ app/src/main/java/com/hk/trendingrepos/MainActivity.kt
@@ -5,16 +5,14 @@ import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.hk.trendingrepos.databinding.ActivityMainBinding
-import com.hk.trendingrepos.model.Repository
 import com.hk.trendingrepos.model.RepositoryDto
 import com.hk.trendingrepos.presenter.MainActivityPresenter
 import com.hk.trendingrepos.view.RepositoryAdapter
 
-class MainActivity : AppCompatActivity(),MainActivityPresenter.ViewCallBack {
+class MainActivity : AppCompatActivity(), MainActivityPresenter.ViewCallBack {
     private lateinit var adapter: RepositoryAdapter
     private lateinit var presenter: MainActivityPresenter
     private lateinit var binding: ActivityMainBinding
@@ -31,10 +29,17 @@ class MainActivity : AppCompatActivity(),MainActivityPresenter.ViewCallBack {
             repositoryList.clear()
             repositoryList.addAll(it)
             adapter.notifyDataSetChanged()
-            if(it.isNullOrEmpty())
+            if (it.isNullOrEmpty())
                 presenter.getRepositoriesFromServer()
 
         })
+
+        binding.swipeRefresh.setOnRefreshListener {
+
+            // This method performs the actual data-refresh operation.
+            // The method calls setRefreshing(false) when it's finished.
+            presenter.getRepositoriesFromServer()
+        }
 
     }
 
@@ -47,20 +52,28 @@ class MainActivity : AppCompatActivity(),MainActivityPresenter.ViewCallBack {
     }
 
     override fun showProgressBar() {
+        binding.swipeRefresh.isRefreshing = true
         binding.shimmerViewContainer.visibility = View.VISIBLE
+        binding.repositoryRv.visibility = View.GONE
+        binding.retryView.visibility = View.GONE
     }
 
     override fun hideProgressBar() {
+        binding.swipeRefresh.isRefreshing = false
         binding.shimmerViewContainer.stopShimmer()
         binding.shimmerViewContainer.visibility = View.GONE
+        binding.retryView.visibility = View.GONE
+        binding.repositoryRv.visibility = View.VISIBLE
     }
 
     override fun onRepositoryFetchFailed() {
-        binding.retryView.visibility = View.VISIBLE
-        val retryImageView = findViewById<ImageView>(R.id.retry_image_view)
-        Glide.with(this).load(R.drawable.retry_gif).into(retryImageView)
-        binding.retryTv.setOnClickListener{
-            presenter.getRepositories()
+        if(repositoryList.isNullOrEmpty()) {
+            binding.retryView.visibility = View.VISIBLE
+            val retryImageView = findViewById<ImageView>(R.id.retry_image_view)
+            Glide.with(this).load(R.drawable.retry_gif).into(retryImageView)
+            binding.retryButton.setOnClickListener {
+                presenter.getRepositoriesFromServer()
+            }
         }
     }
 }
